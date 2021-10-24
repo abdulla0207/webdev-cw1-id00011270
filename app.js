4aafd308bc4878025c3b0f3605ad51a6e097ab1e
Abdulla Ermatov <abdulla.ermatov0407@gmail.com>
2021-10-24 10:11:32 +0500

    Design and Functionality of the SearchBox Recommendation part

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const burgerNav = document.querySelector('.burger-navigation');
 const navigation = document.querySelector('.navigation-lists');
+const searchBox = document.querySelector('.search');
+const searchedItemsBox = document.querySelector('.popular-searched-box')
 
 burgerNav.addEventListener('click', function(){
     navigation.classList.toggle('burger-activate')
 });
+
+searchBox.addEventListener('focus', function(){
+    searchedItemsBox.style.display = "block";
+})
+
+searchBox.addEventListener('blur', function(){
+    searchedItemsBox.style.display = "none";
+})
