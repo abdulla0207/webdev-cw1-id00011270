bb4ae3917785c4f1ec26e93fcfab5bce6ea5daaa
Abdulla Ermatov <abdulla.ermatov0407@gmail.com>
2021-10-25 20:56:39 +0500

    Added Introduction and started Aside Part

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const burgerNav = document.querySelector('.burger-navigation');
 const navigation = document.querySelector('.navigation-lists');
 const searchBox = document.querySelector('.search');
-const searchedItemsBox = document.querySelector('.popular-searched-box')
+const searchedItemsBox = document.querySelector('.popular-searched-box');
+const magnifier = document.querySelector('.magnifier-img');
+
 
 burgerNav.addEventListener('click', function(){
-    navigation.classList.toggle('burger-activate')
+    navigation.classList.toggle('burger-activate');
 });
 
 searchBox.addEventListener('focus', function(){
     searchedItemsBox.style.display = "block";
-})
+});
 
 searchBox.addEventListener('blur', function(){
     searchedItemsBox.style.display = "none";
-})
+});
+
+magnifier.addEventListener('click', function(){
+    
+});
