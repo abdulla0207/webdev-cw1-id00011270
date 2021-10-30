c900aa9123a406193e628a77c489038dee649923
Abdulla Ermatov <abdulla.ermatov0407@gmail.com>
2021-10-30 23:26:55 +0500

    Popular Courses Blocks and function

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const burgerNav = document.querySelector('.burger-navigation');
 const navigation = document.querySelector('.navigation-lists');
 const searchBox = document.querySelector('.search');
 const searchedItemsBox = document.querySelector('.popular-searched-box');
-const magnifier = document.querySelector('.magnifier-img');
-
+const popularCoursesTextBlock = document.querySelector('.popular-courses-textbox');
+const popularSectionBlock = document.getElementsByClassName('section-block');
+const article_block = document.getElementsByClassName('article-block');
+const description_pointer = document.getElementsByClassName('description');
 
 burgerNav.addEventListener('click', function(){
     navigation.classList.toggle('burger-activate');
@@ -17,6 +19,17 @@ searchBox.addEventListener('blur', function(){
     searchedItemsBox.style.display = "none";
 });
 
-magnifier.addEventListener('click', function(){
+function showHidePopularCourses(){
+    for (let i = 0; i < popularSectionBlock.length; i++) {
+        if(popularSectionBlock[i].style.display === "none"){
+            popularSectionBlock[i].style.display = "block";
+        }else{
+            popularSectionBlock[i].style.display = "none";
+        }
+    }
+}
+popularCoursesTextBlock.addEventListener('click', showHidePopularCourses);
+
+function descriptionOfCourses(){
     
-});
+}
