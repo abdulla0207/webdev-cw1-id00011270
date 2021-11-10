905a5fc387e1ef1885a91275e6bf173239ca3ce7
abdulla0207 <abdulla.ermatov0407@gmail.com>
2021-11-10 22:45:04 +0500

    Ready sign up function and style

--- signup.js
+++ signup.js
@@ -1,3 +1,14 @@
-const parent_block = document.querySelector('sign-up-parent-block');
+const parent_block = document.getElementById('sign-up-parent-block');
 const sign_up_button = document.getElementById('sign-up');
 const full_name_text = document.getElementById('full-name-input');
+const div_block = document.getElementById('welcome-block');
+
+
+sign_up_button.addEventListener('click', function(){
+    let name = full_name_text.value;
+    parent_block.style.display = "none";
+    div_block.style.display = "inline-block";
+    let heading = document.createElement('h2')
+    heading = document.createTextNode(`WELCOME ${name.toUpperCase()}!`)
+    div_block.appendChild(heading)
+})
