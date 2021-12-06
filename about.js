4bed13f1b805dc2c44be9ae6492a090ac089c399
abdulla0207 <abdulla.ermatov0407@gmail.com>
2021-12-06 15:40:48 +0500

    Added about.js file and working on dark and light mode of the website

---
+++ about.js
@@ -0,0 +1,2 @@
+const input_button = document.getElementById('submit-button');
+
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const popularSectionBlock = document.getElementsByClassName('section-block');
-const popularCoursesTextBlock = document.querySelector('.popular-courses-textbox');
+const popularCoursesTextBlock = document.querySelector('.courses-textbox');
+const dark_btn = document.getElementById('dark-mode');
+const light_btn = document.getElementById('light-mode');
+const headers = [document.getElementById('header'), document.getElementById('header-for-signup')]
+const sign_up_btn = document.querySelector('.nav-link-signup');
+const nav_lists = document.querySelector('.navigation-lists');
+const body = document.getElementsByTagName('BODY');
+const courses_text = document.querySelector('.courses-textbox');
+const free_courses_heading = document.querySelector('.free-courses-heading')
 
 function showHidePopularCourses(){
     for (let i = 0; i < popularSectionBlock.length; i++) {
@@ -11,3 +19,27 @@ function showHidePopularCourses(){
     }
 }
 popularCoursesTextBlock.addEventListener('click', showHidePopularCourses);
+
+function darkModeWhenClicked(){
+    for (let index = 0; index < headers.length; index++) {
+        headers[index].style.backgroundColor = 'rgb(41, 40, 40)';
+        sign_up_btn.style.backgroundColor = 'rgb(56, 55, 55)';
+        nav_lists.style.backgroundColor = 'rgb(41, 40, 40)';
+        body[0].style.backgroundColor = 'rgb(29, 27, 27)';
+        courses_text.style.color = 'rgb(250, 250, 250)';
+        free_courses_heading.style.color = 'rgb(250, 250, 250)';
+    }
+}
+dark_btn.addEventListener('click', darkModeWhenClicked);
+
+function lightModeWhenClicked(){
+    for (let index = 0; index < headers.length; index++) {
+        headers[index].style.backgroundColor = 'rgb(0, 63, 145)';
+        sign_up_btn.style.backgroundColor = 'rgb(71, 71, 253)';
+        nav_lists.style.backgroundColor = 'rgb(0, 63, 145)';
+        body[0].style.backgroundColor = '#e8e8f1';
+        courses_text.style.color = 'rgb(0, 0, 0)';
+        free_courses_heading.style.color = 'rgb(0, 0, 0)';
+    }
+}
+light_btn.addEventListener('click', lightModeWhenClicked);
