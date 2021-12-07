bb34fbc7d4b3875bb9f50fe0f64f0e7be69689af
abdulla0207 <abdulla.ermatov0407@gmail.com>
2021-12-07 23:20:04 +0500

    Finished dark and light mode function for index.html and slight changes

--- javascript/about.js
+++ javascript/about.js
@@ -1,2 +1,19 @@
 const input_button = document.getElementById('submit-button');
+const review_block = document.getElementById('reviews');
+const text = document.getElementById('review-text')
 
+input_button.addEventListener('click', function(e){
+    e.preventDefault();
+    if(!(text.value.length == 0)){
+        let heading = document.createElement("h2")
+        let heading_text = document.createTextNode("Anonymous:"); 
+        heading.appendChild(heading_text)
+        let review = document.createElement('p');
+        let new_line = document.createElement('br');
+        review_block.appendChild(heading);
+        let review_text = document.createTextNode(` ${text.value}`);
+        review.appendChild(review_text);
+        review_block.appendChild(review);
+        review_block.appendChild(new_line);
+    }
+})
--- javascript/index.js
+++ javascript/index.js
@@ -7,11 +7,13 @@ const sign_up_btn = document.querySelector('.nav-link-signup');
 const nav_lists = document.querySelector('.navigation-lists');
 const body = document.getElementsByTagName('BODY');
 const courses_text = document.querySelector('.courses-textbox');
-const free_courses_heading = document.querySelector('.free-courses-heading')
-const footer = document.getElementById('footer')
+const free_courses_heading = document.querySelector('.free-courses-heading');
+const footer = document.getElementById('footer');
+const section_block = document.getElementsByClassName('section-block');
 
 let whiteColor = 'rgb(250, 250, 250)';
 let blackColor = 'rgb(0, 0, 0)';
+let blueColor = 'rgb(0, 63, 145)';
 
 // This function will show and hide the popular courses section in 
 // home page when the Popular Courses is pressed
@@ -36,7 +38,10 @@ function darkModeWhenClicked(){
         body[0].style.backgroundColor = 'rgb(29, 27, 27)';
         courses_text.style.color = whiteColor;
         free_courses_heading.style.color = whiteColor;
-        footer.style.backgroundColor = 'rgb(0, 63, 145)';
+        footer.style.backgroundColor = blueColor;
+        for (let i = 0; i < section_block.length; i++) {
+            section_block[i].style.boxShadow = `0px 1px 7px 3px ${blueColor}`;
+        }
     }
 }
 dark_btn.addEventListener('click', darkModeWhenClicked);
@@ -44,13 +49,16 @@ dark_btn.addEventListener('click', darkModeWhenClicked);
 // This function will change the background and content color to the initial color after the dark mode was on
 function lightModeWhenClicked(){
     for (let index = 0; index < headers.length; index++) {
-        headers[index].style.backgroundColor = 'rgb(0, 63, 145)';
+        headers[index].style.backgroundColor = blueColor;
         sign_up_btn.style.backgroundColor = 'rgb(71, 71, 253)';
-        nav_lists.style.backgroundColor = 'rgb(0, 63, 145)';
+        nav_lists.style.backgroundColor = blueColor;
         body[0].style.backgroundColor = '#e8e8f1';
         courses_text.style.color = blackColor;
         free_courses_heading.style.color = blackColor;
         footer.style.backgroundColor = 'rgb(29, 27, 27)';
+        for (let i = 0; i < section_block.length; i++) {
+            section_block[i].style.boxShadow = '0px 1px 7px 3px rgba(112, 112, 112, 0.9)';
+        }
     }
 }
 light_btn.addEventListener('click', lightModeWhenClicked);
