10f7e62d69338d6041fcd2840bb1d0d5dc88f809
abdulla0207 <abdulla.ermatov0407@gmail.com>
2021-12-08 23:53:02 +0500

    Ready style of js course html file

--- javascript/about.js
+++ javascript/about.js
@@ -1,7 +1,20 @@
 const input_button = document.getElementById('submit-button');
 const review_block = document.getElementById('reviews');
 const text = document.getElementById('review-text')
+var about_description_text = {
+    about_us: "Study∞ was established in 2021. CEO of this Course Website is John Berntall. In this course website want to provide the best quality of courses and best courses to our learners. We are only hiring the best teachers. For example the most famous and promimnet teacher is John Doell, he studied in Harvard University. That is not all, we are also want to become one of the best online teaching website in Uzbekistan.",
+    students: "Our students are one of the best. They are from all over the world and you can communicate with them via group chats. One of the students that took course in our webistes, he learned Java programming language and after that he was provided with the a workplace in Facebook. Also, with them you can share and collaborate and do online courses together."
+}
+const about = document.getElementById('about-p')
+const students = document.getElementById('students-p')
 
+function descriptions(){
+    let about_text = document.createTextNode(about_description_text.about_us);
+    about.appendChild(about_text);
+    let students_text = document.createTextNode(about_description_text.students);
+    students.appendChild(students_text)
+}
+descriptions()
 input_button.addEventListener('click', function(e){
     e.preventDefault();
     if(!(text.value.length == 0)){
--- javascript/js-course.js
+++ javascript/js-course.js
@@ -1,2 +1,13 @@
-
-
+const description = document.getElementById('description-p')
+const text = document.getElementById('text-p')
+var textArray = {
+    description: "This course is absolutely free for everyone. You may ask, why learning JavaScript language is free here, whereas other websites sell it for money? First of all, today every programmer know JavaScript language and as StackOverflow survey states 66% of programmers know this language. Secondly we want to provide the easiest access to learning",
+    text: "In this course, you firstly learn the basics of this language such as Variables, Loops and Conditional Statements. After basics, you will dive in to Objects and Arrays. Be ready, because Objects and Arrays are used in every project and code."
+}
+function textAdd(){
+    let description_text = document.createTextNode(textArray.description)
+    description.appendChild(description_text)
+    let course_text = document.createTextNode(textArray.text)
+    text.appendChild(course_text)
+}
+textAdd();
